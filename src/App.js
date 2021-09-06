1d0a262f48cfd3b5f854e34d11254f4b0f1ef463
Hardik <beladiyahardik755@gmail.com>
2021-09-06 10:15:32 +0530

    Queue with ReactJs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  //Hook
+  const [queue , setQueue] = useState([]);
+  const [queueIs , setQueueIs ] = useState('');
+  const [revArr , setRevArr] = useState([]);
+
+  useEffect(()=>{
+    const notice = document.querySelector('.notice');
+    notice.textContent = ""
+    notice.style.visibility = "hidden";
+  } , [])
+
+  useEffect(()=>{
+    console.log(queue);
+    const reverseItem = queue.map(item => item).reverse();
+    setRevArr(reverseItem);
+
+    const notice = document.querySelector('.notice');
+    notice.style.visibility = "hidden"
+
+  },[queue])
+
+  //Function For Add Queue
+  const queueIt = () =>{
+    if(queueIs !== ''){
+      if(queue.length < 5){
+        setQueue([...queue , queueIs])
+        setQueueIs('');
+      }
+      else{
+        const notice = document.querySelector('.notice');
+        notice.style.visibility = "visible";
+        notice.textContent = "Queue Overflow";
+      }
+    }else{
+      console.log("Enter Queue first");
+      const notice = document.querySelector('.notice');
+      notice.style.visibility = "visible";
+      notice.textContent = "Enter Queue";
+    }
+  }
+
+  //Function For Remove Queue
+  const fireIt = () => {
+    if(queue.length > 0){
+      queue.shift();
+      setQueue([...queue]);
+    }
+    else{
+      const notice = document.querySelector('.notice');
+      notice.style.visibility = "visible";
+      notice.textContent = "Queue Underflow"
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <input type="text" 
+        
+         value={queueIs} placeholder="Enter Queue" onChange={e => setQueueIs(e.target.value)}/>
+        <button onClick={queueIt}>Queue It</button>
+        <button onClick={fireIt}>Fire It</button>
+
+        <div className="queueList">
+          {
+            revArr.map((item)=>(
+              <p className="queueSingle">{item}</p>
+            ))
+          }
+        </div>
+
+      </div>
+        <div className="notice">
+          <div className="noticeText"></div>
+        </div>
     </div>
   );
 }
